0c4b53f2b0af94dfc22f2534abb0c2c26c21f5ad
Ginger1221 <877653250@qq.com>
2023-10-31 16:12:40 -0700

    Midterm finished

--- myproject/demo1/src/main/java/edu/neu/mgen/InClassPrac.java
+++ myproject/demo1/src/main/java/edu/neu/mgen/InClassPrac.java
@@ -2,10 +2,7 @@ package edu.neu.mgen;
 
 public class InClassPrac {
 
-    public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            if (i != 6)
-                System.out.println(i);
-        }
-    }
+  public static void main(String[] args) {
+
+  }
 }
---
+++ myproject/demo1/src/main/java/edu/neu/mgen/Midterm.java
@@ -0,0 +1,24 @@
+package edu.neu.mgen;
+
+public class Midterm {
+    public static int sumOfMatrix(int[][] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                sum += a[i][j];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] midTermMatrix = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        // It could be any size of matrix.
+        System.out.println("The sum of the matrix is " +
+                sumOfMatrix(midTermMatrix));
+    }
+}
