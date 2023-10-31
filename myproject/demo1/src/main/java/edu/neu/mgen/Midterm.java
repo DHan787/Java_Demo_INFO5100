75d3e7a94a39dfcdb518066abeca60d1d792c0b8
Ginger1221 <877653250@qq.com>
2023-10-31 16:15:45 -0700

    midterm Code finished

--- myproject/demo1/src/main/java/edu/neu/mgen/Midterm.java
+++ myproject/demo1/src/main/java/edu/neu/mgen/Midterm.java
@@ -15,7 +15,8 @@ public class Midterm {
         int[][] midTermMatrix = {
                 { 1, 2, 3 },
                 { 4, 5, 6 },
-                { 7, 8, 9 }
+                { 7, 8, 9 },
+                { 10, 11, 12 }
         };
         // It could be any size of matrix.
         System.out.println("The sum of the matrix is " +
